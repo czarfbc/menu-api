0ead2e532e732dca79238133142b600c4acf3b42
Cezar Fernando <czarfbc@gmail.com>
2023-11-07 17:47:36 -0300

    class-validator createDTO

--- src/estabelecimento/dto/create-estabelecimento.dto.ts
+++ src/estabelecimento/dto/create-estabelecimento.dto.ts
@@ -1,4 +1,10 @@
-import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
 
 export class CreateEstabelecimentoDto {
   @MaxLength(255)
@@ -6,7 +12,17 @@ export class CreateEstabelecimentoDto {
   @IsNotEmpty()
   nome: string;
 
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   credencial: string;
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   senha: string;
+
+  @Min(0)
+  @IsNumber()
+  @IsNotEmpty()
   frete: number;
 }
