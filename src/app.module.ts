4dae86fc55730afd30ef81e3befb69fe4a67d6c6
Cezar Fernando <czarfbc@gmail.com>
2023-11-08 15:49:32 -0300

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EstabelecimentoModule } from './estabelecimento/estabelecimento.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [EstabelecimentoModule, PrismaModule],
+  imports: [EstabelecimentoModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
